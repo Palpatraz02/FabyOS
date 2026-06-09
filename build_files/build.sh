2f54f5f28588188a5bed1e3e7db31460184e5a6e
Fabien <fabyd02@outlook.com>
2026-06-09 09:54:19 +0200

    Cosmic and Chrome

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,23 +5,20 @@ set -ouex pipefail
 ## DNF5 Speedup
 sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
 
+## Install cosmic
+
+bash /ctx/build/cosmic.sh
+
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+bash /ctx/build/chrome.sh
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+dnf -y install papirus-icon-theme bibata-cursor-theme
+dnf -y install distrobox tmux
 
 systemctl enable podman.socket
+
+dnf5 -y clean all
+rm -rf /run/dnf /run/selinux-policy
+rm -rf /var/lib/dnf
