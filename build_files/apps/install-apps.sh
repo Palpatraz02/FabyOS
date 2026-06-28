c704d4f116d27f80d06ab506c67988c754fec3b1
Fabien <fabyd02@outlook.com>
2026-06-28 18:40:47 +0200

    Fix proton pass

--- build_files/apps/install-apps.sh
+++ build_files/apps/install-apps.sh
@@ -11,14 +11,9 @@ bash /ctx/apps/cosmic.sh
 ## Install brave
 bash /ctx/apps/brave.sh
 
-## INstall proton
-curl -L -o /tmp/ProtonPass.rpm https://proton.me/download/PassDesktop/linux/x64/ProtonPass.rpm
-dnf -y install /tmp/ProtonPass.rpm
-rm /tmp/ProtonPass.rpm
+bash /ctx/apps/proton.sh
+
+
 
 dnf -y install papirus-icon-theme bibata-cursor-theme
 dnf -y install $(grep -vE '^\s*(#|$)' /ctx/apps/pkgs.txt)
-
-## Install Proton Pass CLI
-export PROTON_PASS_CLI_INSTALL_DIR=/usr/local/bin
-curl -fsSL https://proton.me/download/pass-cli/install.sh | bash
---
+++ build_files/apps/proton.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+## Install Proton Pass CLI
+export PROTON_PASS_CLI_INSTALL_DIR=/usr/local/bin
+mkdir -p "$PROTON_PASS_CLI_INSTALL_DIR"
+if [ -d /etc/sudoers.d ]; then
+    chmod 0755 /etc/sudoers.d
+fi
+curl -fsSL https://proton.me/download/pass-cli/install.sh | bash
+
+## Install proton pass
+curl -L -o /tmp/ProtonPass.rpm https://proton.me/download/PassDesktop/linux/x64/ProtonPass.rpm
+dnf -y install /tmp/ProtonPass.rpm
+rm /tmp/ProtonPass.rpm
